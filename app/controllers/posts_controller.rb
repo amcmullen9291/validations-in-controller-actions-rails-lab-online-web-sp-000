3f0141496e3c826b546d13c308364826f4967853
anthony mcmullen <amcmullen9291@gmail.com>
2021-02-07 15:50:35 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,8 +11,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.valid? == "true"
-    @post.update(post_params)
+    if @post.update(post_params)
 
     redirect_to post_path(@post)
     else 
